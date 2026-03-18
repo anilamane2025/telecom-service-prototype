3a429546a3a66ffd0a51abc02fd8e2c7e37875a9
anilamane2025 <anilworks568@gmail.com>
2026-03-18 11:41:36 +0530

    dev change

--- src/main/java/com/telecom/tsms/service/UserService.java
+++ src/main/java/com/telecom/tsms/service/UserService.java
@@ -3,8 +3,8 @@ package com.telecom.tsms.service;
 public class UserService {
 
     public void createUser(){
-        System.out.println("Creating user in dev branch");
-        System.out.println("Creating user with validation");
+        System.out.println("Temporary change - work in progress");
+        System.out.println("Change from DEV branch");
     }
 
 }
