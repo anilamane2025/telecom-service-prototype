0a65cf681a2b479e1abb1da6608ab88765009fb8
anilamane2025 <anilworks568@gmail.com>
2026-03-18 12:29:26 +0530

    feature change in createUser

--- src/main/java/com/telecom/tsms/service/UserService.java
+++ src/main/java/com/telecom/tsms/service/UserService.java
@@ -3,7 +3,7 @@ package com.telecom.tsms.service;
 public class UserService {
 
     public void createUser(){
-        System.out.println("Temporary change - work in progress");
+        System.out.println("User created from FEATURE branch");
         System.out.println("Change from DEV branch");
     }
 
