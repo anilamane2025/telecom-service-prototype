fc60b5064816a780f480cf380bf9913cd396d6af
anilamane2025 <anilworks568@gmail.com>
2026-03-22 11:13:01 +0530

    added customer request and response dto

---
+++ src/main/java/com/telecom/tsms/dto/CustomerRequest.java
@@ -0,0 +1,32 @@
+package com.telecom.tsms.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerRequest {
+
+    @NotBlank(message = "Customer code is required")
+    @Size(max=30, message = "Customer code must not exceed 30 characters")
+    private String customerCode;
+
+    @NotBlank(message = "Full name is required")
+    @Size(message = "full name must not exceed 100 characters")
+    private String fullName;
+    @NotBlank(message = "Mobile number is required")
+    @Size(max = 15,message = "Mobile number must not exceed 15 characters")
+    @Pattern(regexp = "^[0-9]{10,15}$",message = "Mobile number must contain only digits and be between 10 to 15 digits")
+    private String mobileNumber;
+    @NotBlank(message = "Email is requried")
+    @Size(max=100,message = "Email must not exceed 100 characters")
+    @Email(message = "Invalid email format")
+    private String email;
+    @Size(max=50,message = "state must not exceed 50 characters")
+    private String state;
+
+    @Size(max=30,message = "KYC status must not exceed 30 characters")
+    private String kycStatus;
+
+}
--- src/main/java/com/telecom/tsms/entity/Customer.java
+++ src/main/java/com/telecom/tsms/entity/Customer.java
@@ -19,7 +19,7 @@ public class Customer {
     @Column(name = "customer_code",nullable = false,unique = true,length = 30)
     private String customerCode;
 
-    @Column(name = "full_name",nullable = false,unique = true,length = 100)
+    @Column(name = "full_name",nullable = false,length = 100)
     private String fullName;
 
     @Column(name = "mobile_number", nullable = false,unique = true, length = 15)
