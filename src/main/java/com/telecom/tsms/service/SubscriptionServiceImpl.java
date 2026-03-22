a6bd2d3b3a511017f943ba4279684a7cc4abba8b
anilamane2025 <anilworks568@gmail.com>
2026-03-22 22:22:37 +0530

    completed SubscriptionServiceImpl with business logic and expiry calculation

--- src/main/java/com/telecom/tsms/service/SubscriptionServiceImpl.java
+++ src/main/java/com/telecom/tsms/service/SubscriptionServiceImpl.java
@@ -2,11 +2,15 @@ package com.telecom.tsms.service;
 
 import com.telecom.tsms.dto.SubscriptionRequest;
 import com.telecom.tsms.dto.SubscriptionResponse;
+import com.telecom.tsms.entity.Customer;
 import com.telecom.tsms.entity.Subscription;
+import com.telecom.tsms.entity.TelecomPlan;
+import com.telecom.tsms.exception.ResourceNotFoundException;
 import com.telecom.tsms.repository.CustomerRepository;
 import com.telecom.tsms.repository.SubscriptionRepository;
 import com.telecom.tsms.repository.TelecomPlanRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class SubscriptionServiceImpl implements SubscriptionService{
@@ -25,27 +29,55 @@ public class SubscriptionServiceImpl implements SubscriptionService{
     }
     @Override
     public SubscriptionResponse createSubscription(SubscriptionRequest request) {
-        return null;
+        Customer customer = customerRepository.findById(request.getCustomerId())
+                .orElseThrow(()->new ResourceNotFoundException("Customer not found with id: "+request.getCustomerId()));
+
+        TelecomPlan plan = telecomPlanRepository.findById(request.getPlanId())
+                .orElseThrow(()->new ResourceNotFoundException(("Plan not found with id"+request.getPlanId())));
+
+        LocalDate expiryDate = request.getActivationDate().plusDays(plan.getValidityDays());
+
+        Subscription subscription = Subscription.builder()
+                .customer(customer)
+                .telecomPlan(plan)
+                .activationDate(request.getActivationDate())
+                .expiryDate(expiryDate)
+                .status(request.getStatus())
+                .build();
+
+        Subscription saved = subscriptionRepository.save(subscription);
+
+        return mapToResponse(saved);
     }
 
     @Override
     public List<SubscriptionResponse> getAllSubscriptions() {
-        return null;
+        return subscriptionRepository.findAll()
+                .stream()
+                .map(this :: mapToResponse)
+                .toList();
     }
 
     @Override
     public SubscriptionResponse getSubscriptionById(Long id) {
-        return null;
+        Subscription subscription = subscriptionRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Subscription not found with id: "+id));
+        return mapToResponse(subscription);
     }
 
     @Override
     public List<SubscriptionResponse> getSubscriptionByCustomerId(Long customerId) {
-        return null;
+        return subscriptionRepository.findByCustomerId(customerId)
+                .stream()
+                .map(this :: mapToResponse)
+                .toList();
     }
 
     @Override
     public void deleteSubscription(Long id) {
-
+        Subscription subscription = subscriptionRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Subscription not found with id: "+id));
+        subscriptionRepository.delete(subscription);
     }
 
     private SubscriptionResponse mapToResponse(Subscription sub){
