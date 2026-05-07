3ea3347c5d39be25c967764334586823456f3b2b
anilamane2025 <anilworks568@gmail.com>
2026-05-07 07:20:02 +0530

    added RechargeService interface

---
+++ src/main/java/com/telecom/tsms/service/RechargeService.java
@@ -0,0 +1,14 @@
+package com.telecom.tsms.service;
+
+import com.telecom.tsms.dto.RechargeRequest;
+import com.telecom.tsms.dto.RechargeResponse;
+
+import java.util.List;
+
+public interface RechargeService {
+
+    RechargeResponse processRecharge(RechargeRequest request);
+    List<RechargeResponse> getAllRechargeTransactions();
+    RechargeResponse getRechargeTransactionById(Long id);
+    List<RechargeResponse> getRechargeTransactionByCustomerId(Long id);
+}
